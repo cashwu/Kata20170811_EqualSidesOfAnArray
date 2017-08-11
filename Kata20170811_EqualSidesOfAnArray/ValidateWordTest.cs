c67e4857492e691ccfcfe3aecd25294dc2108986
cash <cash@cashwu.com>
2017-08-11 20:11:13 +0800

    input 1,2,1 test case

--- Kata20170811_EqualSidesOfAnArray/ValidateWordTest.cs
+++ Kata20170811_EqualSidesOfAnArray/ValidateWordTest.cs
@@ -7,8 +7,24 @@ namespace Kata20170811_EqualSidesOfAnArray
     public class ValidateWordTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_1_2_1_should_return_1()
         {
+            FindEvenIndexShouldBe(1, new[] {1, 2, 1});
+        }
+
+        private static void FindEvenIndexShouldBe(int expected, int[] array)
+        {
+            var kata = new Kata();
+            var actual = kata.FindEvenIndex(array);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int FindEvenIndex(int[] arr)
+        {
+            return 1;
         }
     }
 }
