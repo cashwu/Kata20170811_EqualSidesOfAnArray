d39a1b97a3e65d7ebdf89651c48463ede72739bb
cash <cash@cashwu.com>
2017-08-11 20:32:38 +0800

    refactor

--- Kata20170811_EqualSidesOfAnArray/ValidateWordTest.cs
+++ Kata20170811_EqualSidesOfAnArray/ValidateWordTest.cs
@@ -19,6 +19,30 @@ namespace Kata20170811_EqualSidesOfAnArray
             FindEvenIndexShouldBe(2, new[] { 1, 2, 3, 2, 1 });
         }
 
+        [TestMethod]
+        public void input_1_2_3_4_3_2_1_should_return_3()
+        {
+            FindEvenIndexShouldBe(3, new[] { 1, 2, 3, 4, 3, 2, 1 });
+        }
+
+        [TestMethod]
+        public void input_1_100_50_n51_1_1_should_return_3()
+        {
+            FindEvenIndexShouldBe(1, new[] { 1, 100, 50, -51, 1, 1 });
+        }
+
+        [TestMethod]
+        public void input_1_2_3_4_5_6_should_return_n1()
+        {
+            FindEvenIndexShouldBe(-1, new[] { 1, 2, 3, 4, 5, 6 });
+        }
+
+        [TestMethod]
+        public void input_20_10_30_10_10_15_35_should_return_3()
+        {
+            FindEvenIndexShouldBe(3, new[] { 20, 10, 30, 10, 10, 15, 35 });
+        }
+
         private static void FindEvenIndexShouldBe(int expected, int[] array)
         {
             var kata = new Kata();
@@ -33,10 +57,7 @@ namespace Kata20170811_EqualSidesOfAnArray
         {
             for (var i = 0; i < arr.Length; i++)
             {
-                var leftItemSum = arr.Select((a, idx) => idx < i ? a : 0).Sum();
-                var rightItemSum = arr.Select((a, idx) => idx > i ? a : 0).Sum();
-
-                if (leftItemSum == rightItemSum)
+                if (arr.Take(i).Sum() == arr.Skip(i + 1).Sum())
                 {
                     return i;
                 }
